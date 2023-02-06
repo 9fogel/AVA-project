6e0e52a1aa2aca68fc5a5f59549b5a1274833233
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-02-07 02:00:37 +0300

    feat: add show/hide for tools

--- src/modules/controller/controller.ts
+++ src/modules/controller/controller.ts
@@ -1,38 +1,42 @@
 import Settings from './settings';
+import Editor from './editor';
 
 class Controller {
   private readonly settings: Settings;
+  private readonly editor: Editor;
 
   constructor() {
     this.settings = new Settings();
+    this.editor = new Editor();
   }
 
-  public run() {
+  public run(): void {
     console.log('run controller');
     this.settings.handleSettings();
     this.handleImageUpload();
     this.handleImageDeletion();
+    this.editor.handleEditor();
   }
 
-  private handleImageUpload() {
-    const uploadBtn = document.querySelector('.upload-btn');
+  private handleImageUpload(): void {
+    const uploadBtn: HTMLElement | null = document.querySelector('.upload-btn');
     uploadBtn?.addEventListener('click', () => {
       // TODO: функция модели, которая грузит картинку в контейнер
       this.switchWorkingAreas();
     });
   }
 
-  private handleImageDeletion() {
-    const deleteBtn = document.querySelector('.delete-btn');
+  private handleImageDeletion(): void {
+    const deleteBtn: HTMLElement | null = document.querySelector('.delete-btn');
     deleteBtn?.addEventListener('click', () => {
       // TODO: функция модели, которая убирает картинку из контейнера
       this.switchWorkingAreas();
     });
   }
 
-  private switchWorkingAreas() {
-    const editArea = document.querySelector('.edit-area');
-    const uploadArea = document.querySelector('.upload-area');
+  private switchWorkingAreas(): void {
+    const editArea: HTMLElement | null = document.querySelector('.edit-area');
+    const uploadArea: HTMLElement | null = document.querySelector('.upload-area');
     editArea?.classList.toggle('hidden');
     uploadArea?.classList.toggle('hidden');
   }
--- src/modules/controller/settings.ts
+++ src/modules/controller/settings.ts
@@ -3,15 +3,15 @@ class Settings {
     this.listenSettings();
   }
 
-  private listenSettings() {
-    const settingsIcon = document.querySelector('.settings');
+  private listenSettings(): void {
+    const settingsIcon: HTMLElement | null = document.querySelector('.settings');
     settingsIcon?.addEventListener('click', () => {
       this.showSettings();
       settingsIcon.classList.toggle('active');
     });
 
-    const themeCheckbox = document.querySelector('.theme-checkbox');
-    const toggleWrap = document.querySelector('.toggle-wrap');
+    const themeCheckbox: HTMLElement | null = document.querySelector('.theme-checkbox');
+    const toggleWrap: HTMLElement | null = document.querySelector('.toggle-wrap');
     themeCheckbox?.addEventListener('click', () => {
       console.log('theme changed');
       toggleWrap?.classList.toggle('active');
@@ -19,12 +19,12 @@ class Settings {
     });
   }
 
-  private showSettings() {
-    const settingsMenu = document.querySelector('.settings-menu');
+  private showSettings(): void {
+    const settingsMenu: HTMLElement | null = document.querySelector('.settings-menu');
     settingsMenu?.classList.toggle('hidden');
   }
 
-  private switchTheme() {
+  private switchTheme(): void {
     document.body.classList.toggle('dark-theme');
   }
 }
