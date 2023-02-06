6648750bd1f99c5155e53a154ffbc8a4ae0c0f81
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-02-07 01:27:52 +0300

    feat: add dark theme styles and switch functionality

--- src/modules/controller/controller.ts
+++ src/modules/controller/controller.ts
@@ -10,6 +10,31 @@ class Controller {
   public run() {
     console.log('run controller');
     this.settings.handleSettings();
+    this.handleImageUpload();
+    this.handleImageDeletion();
+  }
+
+  private handleImageUpload() {
+    const uploadBtn = document.querySelector('.upload-btn');
+    uploadBtn?.addEventListener('click', () => {
+      // TODO: функция модели, которая грузит картинку в контейнер
+      this.switchWorkingAreas();
+    });
+  }
+
+  private handleImageDeletion() {
+    const deleteBtn = document.querySelector('.delete-btn');
+    deleteBtn?.addEventListener('click', () => {
+      // TODO: функция модели, которая убирает картинку из контейнера
+      this.switchWorkingAreas();
+    });
+  }
+
+  private switchWorkingAreas() {
+    const editArea = document.querySelector('.edit-area');
+    const uploadArea = document.querySelector('.upload-area');
+    editArea?.classList.toggle('hidden');
+    uploadArea?.classList.toggle('hidden');
   }
 }
 
--- src/modules/controller/settings.ts
+++ src/modules/controller/settings.ts
@@ -9,11 +9,23 @@ class Settings {
       this.showSettings();
       settingsIcon.classList.toggle('active');
     });
+
+    const themeCheckbox = document.querySelector('.theme-checkbox');
+    const toggleWrap = document.querySelector('.toggle-wrap');
+    themeCheckbox?.addEventListener('click', () => {
+      console.log('theme changed');
+      toggleWrap?.classList.toggle('active');
+      this.switchTheme();
+    });
   }
 
   private showSettings() {
     const settingsMenu = document.querySelector('.settings-menu');
     settingsMenu?.classList.toggle('hidden');
+  }
+
+  private switchTheme() {
+    document.body.classList.toggle('dark-theme');
   }
 }
 
