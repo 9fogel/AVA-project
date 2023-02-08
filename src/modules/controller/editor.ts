760e3dcb4303be32bbd2857f1bef6473b59a0d13
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-02-09 02:17:05 +0300

    feat: add flip-rotate controls, style and click behaviour

--- src/modules/controller/editor.ts
+++ src/modules/controller/editor.ts
@@ -36,7 +36,8 @@ class Editor {
 
   private listenToolOptions(): void {
     this.listenResize();
-    //TODO сюда добавятся методы и на другие опции (flip/rotate, brightness и т.д)
+    this.listenFlipAndRotate();
+    //TODO сюда добавятся методы и на другие опции
   }
 
   private showToolOptionsList(toolName: string): void {
@@ -89,13 +90,12 @@ class Editor {
     });
   }
 
-  private listenLockProportions() {
+  private listenLockProportions(): void {
     const propControl = document.querySelector('.proportions');
     propControl?.addEventListener('click', () => {
       propControl.classList.toggle('locked');
       //TODO: передать в модель инфу, что пропорции для resize разлочены/залочены
     });
-    console.log('listen proportions');
   }
 
   private enableDoneBtn(): void {
@@ -111,6 +111,36 @@ class Editor {
       //TODO: отсюда передавать значения инпутов или потом в модели их тянуть?)
       //TODO: подумать нужно ли после применения изменений закрывать все менюшки и выдавать какое-то уведомление, что Image has been resized
       //TODO: если всё закрываем, то в resize переписываем инпуты на новые размеры и лочим кнопку Готово?
+    });
+  }
+
+  //_______________________________________________FLIP & ROTATE
+  private listenFlipAndRotate(): void {
+    const flipVert = document.querySelector('.flip-vert');
+    const flipHor = document.querySelector('.flip-hor');
+    const rotateLeft = document.querySelector('.rotate-left');
+    const rotateRight = document.querySelector('.rotate-right');
+
+    flipVert?.addEventListener('click', () => {
+      console.log('flip vertically');
+      //TODO: метод модели, который отражает по вертикали
+    });
+
+    flipHor?.addEventListener('click', () => {
+      console.log('flip horizontally');
+      //TODO: метод модели, который отражает по горизонтали
+    });
+
+    rotateLeft?.addEventListener('click', () => {
+      console.log('rotate left');
+      //TODO: метод модели, который разворачивает против часов стрелки
+      //TODO: один клик = 90 градусов, каждый следующий клик ещё 90 градусов
+    });
+
+    rotateRight?.addEventListener('click', () => {
+      console.log('rotate right');
+      //TODO: метод модели, который разворачивает по часов стрелке
+      //TODO: один клик = 90 градусов, каждый следующий клик ещё 90 градусов
     });
   }
 }
