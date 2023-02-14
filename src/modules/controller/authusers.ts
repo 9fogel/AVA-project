3a65cf9b17a6f0ebaa1a95d20b2b4b9756035fc9
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-02-14 15:30:16 +0300

    feat: setup registration user

--- src/modules/controller/authusers.ts
+++ src/modules/controller/authusers.ts
@@ -2,6 +2,7 @@ import AuthModel from '../model/auth';
 
 class UsersControler {
   users: AuthModel;
+  JWT = localStorage.setItem('JWT', '');
   constructor() {
     this.users = new AuthModel();
   }
@@ -17,16 +18,62 @@ class UsersControler {
   }
 
   handleRegistrationUser() {
-    document.querySelector('.modal-login-btn')?.addEventListener('click', () => {
-      return this.users.registrationUser('Anjjfg3', 'admin');
+    const userName = document.querySelector('#user-name') as HTMLInputElement;
+    const userEmail = document.querySelector('#email') as HTMLInputElement;
+    const password = document.querySelector('#password') as HTMLInputElement;
+    const repeatPassword = document.querySelector('#undefined') as HTMLInputElement;
+
+    password.addEventListener('input', () => {
+      const messagePassword = document.querySelector('.confirm-password-message');
+      if (messagePassword) {
+        if (password.value !== repeatPassword.value) {
+          messagePassword.textContent = 'Password mismatch';
+        } else {
+          messagePassword.textContent = 'Password match';
+        }
+      }
+    });
+
+    if (password.value === repeatPassword.value) {
+      document.getElementById('sign-in-google')?.addEventListener('click', async () => {
+        //await this.users.registrationUser(userName.value, userEmail.value, password.value);
+        const data = await this.users.registrationUser(userName.value, userEmail.value, password.value);
+
+        if (data.token) {
+          this.logIn(userName.value, '.login-btn');
+        }
+      });
+    }
+    // document.getElementById('sign-in-google')?.addEventListener('click', async () => {
+    //   const data = await this.users.registrationUser(userName.value, userEmail.value, password.value);
+
+    //   if (data.token) {
+    //     this.logIn(userName.value, '.login-btn');
+    //   }
+    // });
+  }
+
+  async handleLoginUser() {
+    document.getElementById('sign-in')?.addEventListener('click', () => {
+      console.log('click');
+      this.users.logInUser('Anjjfg367', 'Anjjfg3', 'admin');
     });
   }
 
-  handleLoginUser() {
-    document.querySelector('.modal-login-btn')?.addEventListener('click', () => {
-      console.log('click');
-      return this.users.logInUser('Anjjfg3', 'admin');
-    });
+  private logIn(userName: string, button: string) {
+    const buttonName = document.querySelector(button);
+    if (buttonName) {
+      buttonName.textContent = userName;
+    }
+    document.querySelector('.login-modal')?.classList.remove('active');
+  }
+
+  private logOut() {
+    const buttonName = document.querySelector('.login-btn');
+    if (buttonName) {
+      buttonName.textContent = 'Sign In / Sign Up';
+      this.JWT = localStorage.setItem('JWT', '');
+    }
   }
 }
 
--- src/modules/model/auth.ts
+++ src/modules/model/auth.ts
@@ -1,3 +1,14 @@
+interface ValidError {
+  errors: {
+    errors: Array<{ value?: string; msg: string; param: string; location: string }>;
+  };
+}
+
+function findIndexError(error: ValidError, value: string): number {
+  const arr1 = error.errors.errors;
+  return arr1.findIndex((el) => el.param === value);
+}
+
 class AuthModel {
   async getUsers() {
     const response = await fetch('http://localhost:5000/auth/users/', {
@@ -22,7 +33,7 @@ class AuthModel {
     //return;
   }
 
-  async registrationUser(username: string, password: string) {
+  async registrationUser(username: string, userEmail: string, password: string) {
     const response = await fetch('http://localhost:5000/auth/registration/', {
       method: 'POST',
       headers: {
@@ -30,7 +41,7 @@ class AuthModel {
 
         Accept: 'application/json',
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ username, userEmail, password }),
     });
 
     //userName, password,
@@ -39,11 +50,33 @@ class AuthModel {
     if (!response.ok) {
       //throw Error(`Памылка ${response.status}`);
       console.error(`Erros:${response.status}`, data);
+      const messageUser = document.querySelector('.user-message');
+      const messageEmail = document.querySelector('.email-message');
+      const messagePassword = document.querySelector('.password-message');
+
+      if (messageUser) {
+        messageUser.textContent = String(
+          JSON.stringify(data.message || data.errors?.errors[findIndexError(data, 'username')]?.msg) || '',
+        );
+      }
+
+      if (messageEmail) {
+        messageEmail.textContent = String(
+          JSON.stringify(data.errors?.errors[findIndexError(data, 'userEmail')]?.msg) || '',
+        );
+      }
+
+      if (messagePassword) {
+        messagePassword.textContent = String(
+          JSON.stringify(data.errors?.errors[findIndexError(data, 'password')]?.msg) || '',
+        );
+      }
     }
     console.log(JSON.stringify(data));
+    return data;
   }
 
-  async logInUser(username: string, password: string) {
+  async logInUser(username: string, userEmail: string, password: string) {
     const response = await fetch('http://localhost:5000/auth/login/', {
       method: 'POST',
       headers: {
@@ -51,14 +84,19 @@ class AuthModel {
 
         Accept: 'application/json',
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ username, userEmail, password }),
     });
 
     const data = await response.json();
     if (!response.ok) {
       console.error(`Erros:${response.status}`, data);
+      const messageUser = document.querySelector('.user-message');
+      if (messageUser) {
+        messageUser.textContent = String(JSON.stringify(data.message || data.message.errors.errors.msg));
+      }
     }
     console.log(JSON.stringify(data));
+    return data;
   }
 }
 
